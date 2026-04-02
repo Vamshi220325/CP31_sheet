3bd72839b74a39a33276a69afdd51bf684f90904
vamshi <yenaganti.vamshi007@gmail.com>
2026-04-02 20:34:31 +0530

    my first cp problem

---
+++ 800_rated/1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<long long>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    bool flag=true;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<arr[i-1]){flag=false;break;}
+    }
+    if(flag||k>=2)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+int main()
+{  int t;
+    cin>>t;
+    while(t--)
+    {
+
+      solve();
+
+
+    }
+    return 0;
+}
